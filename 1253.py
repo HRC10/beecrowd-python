349ff2cbb23d84bea973d03a25af815a777be649
HRC10 <hugo.castor15@gmail.com>
2026-03-11 13:22:24 -0400

    apenas ajustar o print

--- 1253.py
+++ 1253.py
@@ -1,15 +1,16 @@
 Ntestes = int(input())
 i = 0
 mensagem_final = ''
-palavras = []
+palavras = [0] * Ntestes
 while i < Ntestes:
     mensagem = input()
     chave = int(input())
+    mensagem_final = ''
     for letras in mensagem:
         nova_letra = (ord(letras) - chave)
         if (nova_letra < 65):
             nova_letra = nova_letra + 26
         mensagem_final += chr(nova_letra)
+        palavras[i] = mensagem_final
     i += 1
 print(palavras)
-
