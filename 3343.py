737c0ae2b9f92cf6430e511e713f05f438c1f9b6
HRC10 <hugo.castor15@gmail.com>
2026-03-13 12:22:28 -0400

    3343 em construção

--- 3343.py
+++ 3343.py
@@ -4,3 +4,17 @@ input = sys.stdin.readline
 Ntitas, tamanhoMuralha = map(int, input().split())
 Titas = input()
 P, M, G = map(int, input().split())
+Muros = 0
+Muralha = tamanhoMuralha
+Muros = 1
+for ataque in Titas:
+    if ataque == 'P':
+        if Muralha - P >= P or Muralha - P == 0:
+            Muralha = Muralha - P
+            print("atacou, VIDA", Muralha)
+        elif Muralha == 0 or Muralha - P < P:
+            print("Pulou VIDA", Muralha)
+            Muros += 1
+            Muralha = tamanhoMuralha
+            print(Muros, "MURO NOVO")
+print(Muros)
